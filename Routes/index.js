3d6a04c165653b3fb92dd4a79facb22a4b2582de
LangPiñeiro <alangp@miumg.edu.gt>
2023-05-02 12:05:45 -0600

    cambios, Rutas Para Agregar Beneficiarios

--- Routes/index.js
+++ Routes/index.js
@@ -3,9 +3,11 @@ const router = express();
 const usuarios = require('./usuarios.route');
 const roles = require('./roles.route');
 const auth = require('./auth.route');
+const beneficiarios = require('./beneficiarios.route');
 //this route will come with all comunidades
 router.use('/usuarios',usuarios);
 router.use('/roles', roles );
 router.use('/auth', auth);
+router.use('/beneficiarios', beneficiarios);
 
 module.exports = router; 
