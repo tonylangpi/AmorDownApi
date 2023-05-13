7d3ea1d25c4cd89a2d93b1e4df2cb0bf2dc5adc5
Daniel Bolaños <danielbolanos909@gmail.com>
2023-05-13 15:41:27 -0600

    Se modifico las rutas de los reportes

--- Routes/reportes.route.js
+++ Routes/reportes.route.js
@@ -4,6 +4,6 @@ const router = express();
 const {sesionsForAreas,sesionsForBeneficiary } = require('../Controllers/reportes.controller');
 
 //rutas para traer data de reportes en store procedures de base de datos
-router.get('/sesionesPorArea/:idUsuario/:idEmpresa',sesionsForAreas);
-router.get('/sesionesPorBeneficiario/:idEmpresa',sesionsForBeneficiary);
+router.get('/sesionesPorArea/:idUsuario/:idEmpresa/:fecha_desde/:fecha_hasta',sesionsForAreas);
+router.get('/sesionesPorBeneficiario/:idBeneficiario/:idEmpresa/:fecha_desde/:fecha_hasta',sesionsForBeneficiary);
 module.exports = router;
--- Routes/sesiones.route.js
+++ Routes/sesiones.route.js
@@ -4,7 +4,7 @@ const {sesionesUpload} = require('../services/multer.services');
 const {allSesiones, createSesiones, updateSesiones, deleteSesiones} = require('../Controllers/sesiones.controller');
 
 
-router.get('/',allSesiones);
+router.get('/AreaSesiones/:idArea',allSesiones);
 router.post('/createSesion',sesionesUpload.single('evaluaciones'),createSesiones)
 router.put('/updateSesion', updateSesiones);
 router.delete('/deleteSesion', deleteSesiones);
