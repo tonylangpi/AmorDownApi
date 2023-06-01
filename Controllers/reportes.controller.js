6bade61cb546e4fa8a625bf1bfaac81396041821
LangPiñeiro <alangp@miumg.edu.gt>
2023-06-01 11:48:44 -0600

    cambios, Añadi ruta para reporte Cuantitativo y F9

--- Controllers/reportes.controller.js
+++ Controllers/reportes.controller.js
@@ -83,7 +83,109 @@ const sesionsForBeneficiary = async(req,res) =>{
         res.json(error);
     }
 }
+
+const reporteCuantitativo = (req,res) =>{
+    const{desde, hasta} = req.body; 
+    const formatoFecha = /^\d{4}-\d{2}-\d{2}$/;//regex para validar que la fecha venga año mes dia.
+    try {
+      if(formatoFecha.test(desde) && formatoFecha.test(hasta)){
+        connection.query(`
+        SELECT 
+        CONCAT(B.NOMBRE1, ' ', B.NOMBRE2)AS NOMBRES,
+        CONCAT(B.APELLIDO1, ' ', B.APELLIDO2, ' ') AS APELLIDOS, 
+        B.SEXO, 
+        TIMESTAMPDIFF(YEAR, B.FECHA_NACIMIENTO, 
+        CURDATE()) AS EDAD,
+        CASE 
+        WHEN TIMESTAMPDIFF(YEAR, B.FECHA_NACIMIENTO, CURDATE()) > 18 THEN 'ADULTO'
+        ELSE 'NIÑO'  
+        END AS 'CLASIFICACION',
+        HC.DIAGNOSTICO, HC.DISCAPACIDAD, COUNT(SB.ID_SESION_BENEFICIARIO) AS ASISTENCIA
+        FROM SESIONES_BENEFICIARIO SB
+        INNER JOIN BENEFICIARIO B ON SB.ID_BENEFICIARIO = B.ID_BENEFICIARIO
+        INNER JOIN HISTORIAL_CLINICO HC ON B.ID_BENEFICIARIO = HC.ID_BENEFICIARIO 
+        WHERE SB.FECHA >= ? AND SB.FECHA <= ?
+        GROUP BY NOMBRES, APELLIDOS, B.SEXO, EDAD, CLASIFICACION, DIAGNOSTICO, DISCAPACIDAD `,[desde,hasta], (error,results) =>{
+            if(error){
+                console.log(error);
+            }else{
+                res.json(results);
+            }
+        });
+      }else{
+        res.json({mensaje:"formato de fecha incorrecto"});
+      }
+       
+    } catch (error) {
+        res.send(error); 
+    }
+}
+const reporteF9 = (req,res) =>{
+     const{desde, hasta} = req.body; 
+    const formatoFecha = /^\d{4}-\d{2}-\d{2}$/;//regex para validar que la fecha venga año mes dia.
+    try {
+        connection.query(`
+        CREATE TEMPORARY TABLE IF NOT EXISTS INFORME AS (
+            SELECT SB.FECHA, 
+            B.REFERENCIA, 
+            CONCAT(B.NOMBRE1, ' ', B.NOMBRE2) AS NOMBRES,
+            CONCAT(B.APELLIDO1, ' ', B.APELLIDO2, ' ') AS APELLIDOS, 
+            B.DIRECCION, 
+            B.SEXO,
+            TIMESTAMPDIFF(YEAR, B.FECHA_NACIMIENTO, CURDATE()) AS EDAD, 
+            HC.DISCAPACIDAD, 
+            B.ESCOLARIDAD, 
+            SB.TIPO_SESION, 
+            HC.DIAGNOSTICO, 
+            A.NOMBRE AS SERVICIO_RECIBIDO
+            FROM SESIONES_BENEFICIARIO SB
+            INNER JOIN BENEFICIARIO B ON SB.ID_BENEFICIARIO = B.ID_BENEFICIARIO
+            INNER JOIN HISTORIAL_CLINICO HC ON B.ID_BENEFICIARIO = HC.ID_BENEFICIARIO 
+            INNER JOIN USUARIOS U ON SB.ID_USUARIO = U.ID
+            INNER JOIN AREAS_USUARIOS AU ON U.ID = ID_USUARIO
+            INNER JOIN AREAS A ON AU.ID_AREA = A.ID_AREA
+            GROUP BY FECHA, REFERENCIA, NOMBRES, APELLIDOS, DIRECCION, SEXO, EDAD, DISCAPACIDAD, ESCOLARIDAD,TIPO_SESION, DIAGNOSTICO, SERVICIO_RECIBIDO)
+        `,(error,results) =>{
+            if(error){
+                console.log(error);
+            }else{
+                if(formatoFecha.test(desde) && formatoFecha.test(hasta)){
+                    connection.query(`
+                        SELECT 
+                        GROUP_CONCAT(DAY(FECHA) SEPARATOR ', ') AS FECHA_DIAS,
+                        REFERENCIA, 
+                        NOMBRES, 
+                        APELLIDOS, 
+                        DIRECCION, 
+                        SEXO, 
+                        EDAD, 
+                        DISCAPACIDAD, 
+                        ESCOLARIDAD, 
+                        GROUP_CONCAT(TIPO_SESION SEPARATOR ', ') AS TIPO_SESION,
+                        DIAGNOSTICO, 
+                        GROUP_CONCAT(SERVICIO_RECIBIDO SEPARATOR ', ') AS SERVICIO_RECIBIDO  
+                        FROM INFORME
+                        WHERE FECHA >= ? AND FECHA <= ?
+                        GROUP BY NOMBRES,REFERENCIA, APELLIDOS, DIRECCION, SEXO, EDAD, DISCAPACIDAD, ESCOLARIDAD, DIAGNOSTICO `,[desde,hasta], (error,results) =>{
+                        if(error){
+                            console.log(error);
+                        }else{
+                            res.json(results);
+                        }
+                    });
+                  }else{
+                    res.json({mensaje:"formato de fecha incorrecto"});
+                  }
+            }
+        });
+    } catch (error) {
+        res.send(error); 
+    }
+}
+
 module.exports = {
     sesionsForAreas,
-    sesionsForBeneficiary
+    sesionsForBeneficiary,
+    reporteCuantitativo,
+    reporteF9
 }
--- Routes/reportes.route.js
+++ Routes/reportes.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express();
 
-const {sesionsForAreas,sesionsForBeneficiary } = require('../Controllers/reportes.controller');
+const {sesionsForAreas,sesionsForBeneficiary,reporteCuantitativo, reporteF9 } = require('../Controllers/reportes.controller');
 
 //rutas para traer data de reportes en store procedures de base de datos
 router.get('/sesionesPorArea/:token/:fecha_desde/:fecha_hasta',sesionsForAreas);
 router.get('/sesionesPorBeneficiario/:idBeneficiario/:token/:fecha_desde/:fecha_hasta',sesionsForBeneficiary);
+router.get('/reporteCuantitativo',reporteCuantitativo);
+router.get('/reporteF9',reporteF9);
 module.exports = router;
