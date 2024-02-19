9e5796fd7811d68db2d3d0b7d8021a509a552532
Choper03 <csutucdeleon@gmail.com>
2024-02-18 18:44:56 -0600

    Nuevo servidor BD

--- Database/bd.js
+++ Database/bd.js
@@ -1,20 +1,51 @@
 const { Sequelize } = require('sequelize');
-const mysql = require('mysql2')
+const fs = require('fs');
+const path = require('path');
+const mysql = require('mysql2');
 require('dotenv').config();
-const {DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_NAME,
-    DB_PORT} = process.env;
 
-const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`); 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-const connection = mysql.createConnection(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`); 
+const config = {
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, 'ca.pem')).toString(),
+    rejectUnauthorized: true,
+  }
+};
 
- sequelize.authenticate().then(() => {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(path.resolve(__dirname, 'ca.pem')).toString(),
+      rejectUnauthorized: true,
+    }
+  }
+});
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, 'ca.pem')).toString(),
+    rejectUnauthorized: true,
+  }
+});
+
+sequelize.authenticate().then(() => {
   console.log('Conexion satisfactoria a mysql');
 }).catch((error) => {
   console.error('no se pudo conectar ', error);
 });
 
-module.exports = {sequelize, connection};
+module.exports = { sequelize, connection };
